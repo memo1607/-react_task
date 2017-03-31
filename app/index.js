b31dd284da140a8d6c987916c6a68e2e6423f005
memo <memo816@qq.com>
2017-03-31 21:45:05 +0800

    引入$

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
+import j from 'jquery'
 function component(){
-    var element = document.createElement('div');
+    var element = j('<div></div>');
 
-    element.innerHTML = _.join(['Hello','webpack'],'');
+    element.html( _.join(['Hello','webpack'],'')) ;
 
-    return element;
+    return element.get(0);
 }
 
 document.body.appendChild(component());

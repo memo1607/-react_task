d22b4d11355c173b4a6ad90ed4173bd299fed3f3
memo <memo816@qq.com>
2017-04-01 16:50:27 +0800

    add foo

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
-import j from 'jquery'
+import j from 'jquery';
+import foo from './foo';
 function component(){
     var element = j('<div></div>');
 
-    element.html( _.join(['Hello','webpack'],'')) ;
+    element.html( _.join(['Hello','webpack','!'],'')) ;
 
     return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(foo)
+console.log(foo())

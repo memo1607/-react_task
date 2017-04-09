1914559d4db0fa7c8d67fae6a4e37e421e6575fc
memo <memo816@qq.com>
2017-04-09 22:31:11 +0800

    x

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import Welcome from './Welcome'
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <Welcome/>,
-  document.getElementById('root')
-)
+setInterval(tick, 1000);

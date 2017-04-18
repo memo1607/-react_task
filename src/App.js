9d9798bb3fc5a115a17dedb3267d480eabc082dd
memo <memo816@qq.com>
2017-04-18 18:17:57 +0800

    add leanCloud.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ import './App.css';
 import TodoInput from './TodoInput';
 import {TodoItem} from './TodoItem';
 import * as localStore from './localStore';
-import AV from 'leancloud-storage'
 
-var APP_ID = 'Rj0lqbOGihz4if8N47pyurCF-gzGzoHsz';
-var APP_KEY = 'iIyi8cOi3KXAXdL5KbAETrFa';
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
 
 
 
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'Rj0lqbOGihz4if8N47pyurCF-gzGzoHsz';
+var APP_KEY = 'iIyi8cOi3KXAXdL5KbAETrFa';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
